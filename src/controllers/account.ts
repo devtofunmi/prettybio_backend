6728d8fda98b10a78ff61f403669256cb71e48dd
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-04-10 18:55:40 +0100

    cleanup code

--- src/controllers/account.ts
+++ src/controllers/account.ts
@@ -23,14 +23,37 @@ export const getAccount = async (c: Context) => {
   return c.json({ user });
 };
 
+
+
 export const updateAccount = async (c: Context) => {
   const userId = c.get("userId");
   const { name, bio, image, user_link_name } = await c.req.json();
 
-  const updatedUser = await prisma.user.update({
-    where: { id: userId },
-    data: { name, bio, image, user_link_name },
-  });
+  try {
+    const updatedUser = await prisma.user.update({
+      where: { id: userId },
+      data: {
+        name,
+        bio,
+        image,
+        user_link_name,
+        setup_complete: true,
+      },
+    });
 
-  return c.json({ user: updatedUser });
+    return c.json({
+      message: "Account updated successfully",
+      user: {
+        id: updatedUser.id,
+        username: updatedUser.username,
+        name: updatedUser.name,
+        bio: updatedUser.bio,
+        image: updatedUser.image,
+        user_link_name: updatedUser.user_link_name,
+      },
+    });
+  } catch (err) {
+    console.error("Update Error:", err);
+    return c.json({ error: "Could not update account" }, 500);
+  }
 };
