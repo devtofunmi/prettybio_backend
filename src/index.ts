3fb092ea11fb80230e0d8164d1ef4bd693022f41
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-04-18 13:04:23 +0100

    change the port number

--- src/index.ts
+++ src/index.ts
@@ -41,7 +41,7 @@ app.route('/public/sociallinks', socialPublicRoutes);
 
 serve({
   fetch: app.fetch,
-  port: 3000
+  port: 3001
 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`)
 })
