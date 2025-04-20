19acee142c20789515a09f9faad89a2bf50d64f2
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-04-20 18:18:02 +0100

    ...

--- src/controllers/userpreference.ts
+++ src/controllers/userpreference.ts
@@ -2,17 +2,21 @@ import type { Context } from "hono";
 import { prisma } from "../lib/prisma.js";
 
 export const getUserPreferences = async (c: Context) => {
-  const userId = c.get('userId');
+  const userId = c.req.param('userId'); 
   const user = await prisma.user.findUnique({
     where: { id: userId },
     select: { theme: true, socialPosition: true }
   });
 
+  if (!user) {
+    return c.json({ message: 'User not found' }, 404);
+  }
+
   return c.json(user);
 };
 
 export const updateUserPreferences = async (c: Context) => {
-  const userId = c.get('userId');
+  const userId = c.req.param('userId'); 
   const { theme, socialPosition } = await c.req.json();
 
   const updated = await prisma.user.update({
@@ -22,4 +26,5 @@ export const updateUserPreferences = async (c: Context) => {
 
   return c.json({ message: 'Preferences updated', data: updated });
 };
+
 
--- src/routes/userpreference.ts
+++ src/routes/userpreference.ts
@@ -1,17 +1,14 @@
-// routes/preferences.ts
 import { Hono } from 'hono';
 import { getUserPreferences, updateUserPreferences } from '../controllers/userpreference.js';
 import { authMiddleware } from '../middleware/authmiddleware.js';
 
 export const preferencesRoutes = new Hono();
 
-// Use middleware for all routes in this group
 preferencesRoutes.use('*', authMiddleware);
 
-// GET /preferences
-preferencesRoutes.get('/', getUserPreferences);
+preferencesRoutes.get('/:userId', getUserPreferences);
 
-// PUT /preferences
-preferencesRoutes.put('/', updateUserPreferences);
+preferencesRoutes.put('/:userId', updateUserPreferences);
+
 
 
