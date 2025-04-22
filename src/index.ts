64757387f240eaee7ee8a1551b67b38ac192f41a
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-04-22 19:39:43 +0100

    ..

--- src/index.ts
+++ src/index.ts
@@ -18,34 +18,19 @@ const app = new Hono()
 //   origin: '*' 
 // }));
 
-// app.use(
-//   cors({
-//     origin: (origin) => {
-//       const allowedOrigins = [
-//         'http://localhost:3000',
-//         'https://prettybioo.up.railway.app',
-//       ]
-//       return allowedOrigins.includes(origin ?? '') ? origin : ''
-//     },
-//     credentials: true
-//   })
-// )
-
-const allowedOrigins = [
-  'http://localhost:3000',
-  'https://prettybioo.up.railway.app',
-]
-
 app.use(
-  '*',
   cors({
     origin: (origin) => {
-      if (!origin) return ''
-      return allowedOrigins.includes(origin) ? origin : ''
+      const allowedOrigins = [
+        'http://localhost:3000',
+        'https://prettybioo.up.railway.app',
+      ]
+      return allowedOrigins.includes(origin ?? '') ? origin : ''
     },
-    credentials: true,
+    credentials: true
   })
 )
+
 
 
 app.get('/', (c) => {
